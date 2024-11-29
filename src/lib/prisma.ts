627318140f6db4b6e7872ee61d4330687d0dc7a9
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2024-11-29 11:21:28 +0100

    init turso

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const libsql = createClient({
+	url: `${process.env.TURSO_DATABASE_URL}`,
+	authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+});
 
-export const prisma =
-	globalForPrisma.prisma ||
-	new PrismaClient({
-		log: ["query"],
-	});
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const adapter = new PrismaLibSQL(libsql);
+export const prisma = new PrismaClient();
