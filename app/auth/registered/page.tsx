258c37944c5cbf3dbead22397578e58099e40dc5
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2024-12-05 16:13:35 +0100

    Update register

--- app/auth/registered/page.tsx
+++ app/auth/registered/page.tsx
@@ -1,11 +1,8 @@
 import LogOutButton from "@/src/components/ui/LogOutButton";
 
 export default async function RegisteredPage() {
-	// const session = await auth();
-	// if (!session) return "Sin sesión";
 	return (
 		<div className="h-screen flex flex-col justify-center items-center">
-			{/* <div className="">{JSON.stringify(session)}</div> */}
 			<div className="">Se ha registrado correctamente, por favor verifica tu email para activar tu cuenta</div>
 			<LogOutButton />
 		</div>
