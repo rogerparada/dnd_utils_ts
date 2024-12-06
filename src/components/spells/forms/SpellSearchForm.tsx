2a57c612a11699e0cfe029e4e0ab7260d1cb5258
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2024-12-06 18:54:35 +0100

    Fix search

--- src/components/spells/forms/SpellSearchForm.tsx
+++ src/components/spells/forms/SpellSearchForm.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { SearchSchema } from "@/src/schema";
-import { usePathname, useRouter } from "next/navigation";
-import React from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function SpellSearchForm() {
 	const router = useRouter();
 	const pathName = usePathname();
+	const searchParams = useSearchParams();
+	const params = new URLSearchParams(searchParams);
+
 	const handleSearchForm = (formData: FormData) => {
 		const data = {
 			search: formData.get("search"),
@@ -16,12 +18,12 @@ export default function SpellSearchForm() {
 			result.error.issues.forEach((issue) => console.log(issue.message));
 			return;
 		}
-
-		router.push(`${pathName}/search?search=${result.data.search}`);
+		params.set("search", result.data.search);
+		router.push(`${pathName.includes("/search") ? pathName : pathName + "/search"}?${params.toString()}`);
 	};
 
 	return (
-		<form action={handleSearchForm} className="flex items-center">
+		<form action={handleSearchForm} className="flex items-center text-black">
 			<input type="text" placeholder="Buscar conjuro" className="p-2 placeholder-gray-400 w-full h-12" name="search" />
 			<input type="submit" value="Buscar" className="bg-blue-400 hover:bg-blue-500 uppercase text-white cursor-pointer p-3 text-sm font-black h-12" />
 		</form>
