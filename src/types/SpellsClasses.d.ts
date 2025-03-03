77e053eed09cabcb7293e63fc9988712503130de
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2025-03-03 16:25:18 +0100

    update Types

---
+++ src/types/SpellsClasses.d.ts
@@ -0,0 +1,52 @@
+import { Classes, Spell, SpellClass } from "@prisma/client";
+
+export type Color = {
+	bg: string;
+	text: string;
+	border: string;
+};
+
+export type Colors = {
+	[key: string]: Color;
+};
+
+export type ClassImage = {
+	src: string;
+	alt: string;
+};
+
+export type ClassImages = {
+	[key: string]: ClassImage;
+};
+
+export type Ability = {
+	en: string;
+	es: string;
+	de: string;
+};
+
+export type Abilities = {
+	[key: string]: Ability;
+};
+
+export type FullSpellClass = SpellClass & { clase: Classes };
+
+export type FullSpell = Spell & {
+	classes: FullSpellClass[];
+};
+
+export type SpellClasses = Spell & {
+	classes: SpellClass[];
+};
+
+export type Dice = {
+	faces?: number;
+	value: number;
+	primary?: string;
+	secondary?: string;
+	tertiary?: string;
+	symbols?: string;
+	rolling?: boolean;
+	empty?: boolean;
+	id: number;
+};
