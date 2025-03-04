332eaba4a68635c9ec9f5869b241383099075157
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2025-03-04 17:06:37 +0100

    Change card details style

--- src/components/spells/ListItem.tsx
+++ src/components/spells/ListItem.tsx
@@ -9,8 +9,12 @@ export default function ListItem({ children }: ListItemProps) {
 		const [title, data] = children;
 		return (
 			<li className="mb-2">
-				<details>
-					<summary>{title}</summary>
+				<details className="group">
+					<summary className="cursor-pointer font-bold pl-6 relative flex items-center list-none appearance-none">
+						<span className="icon-[uiw--plus-square-o] absolute left-0 transition-transform group-open:hidden" />
+						<span className="icon-[uiw--minus-square-o] absolute left-0 transition-transform hidden group-open:inline" />
+						{title}
+					</summary>
 					<p className="mt-2">{data}</p>
 				</details>
 			</li>
