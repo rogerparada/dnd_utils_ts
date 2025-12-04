72077063d96ef3e48d4e7304645906b3a48711cc
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2025-12-04 12:49:34 +0100

    feat: Add Prisma ORM integration and generate client files based on schema updates.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,13 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
+import { PrismaClient } from "@/prisma/generated/prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
-const libsql = createClient({
+// const libsql = createClient({
+// 	url: `${process.env.TURSO_DATABASE_URL}`,
+// 	authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+// });
+
+export const adapter = new PrismaBetterSqlite3({
 	url: `${process.env.TURSO_DATABASE_URL}`,
 	authToken: `${process.env.TURSO_AUTH_TOKEN}`,
 });
-
-export const adapter = new PrismaLibSQL(libsql as any);
 export const prisma = new PrismaClient({ adapter });
